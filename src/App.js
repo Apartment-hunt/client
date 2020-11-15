a94b40e2a7430069c90f866191c8e5fd3c1330b4
iamshakibb <shihabunshakib9346@gmail.com>
2020-11-15 14:10:30 +0600

    File init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container, Button } from "react-bootstrap";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Button>Hello</Button>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
