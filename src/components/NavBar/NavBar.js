47bcdde5e494c4213338ef1c6f96dde4fbb8933c
iamshakibb <shihabunshakib9346@gmail.com>
2020-11-16 13:24:01 +0600

    Resolve all the firebase confight

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import "./NabBar.scss";
 import logo from "../../images/logos/Logo.png";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <Container>
       <Navbar expand="md">
-        <Navbar.Brand href="#home" className="logo">
+        <Navbar.Brand as={Link} to="/" className="logo">
           <img src={logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -18,7 +19,9 @@ export default function NavBar() {
             <Nav.Link href="#concerns">Concerns</Nav.Link>
             <Nav.Link href="#event">Event</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
-            <Button className="px-4">Login</Button>
+            <Link to="/login">
+              <Button className="px-4">Login</Button>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
